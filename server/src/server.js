e33dacc557f4003a559e28d5b68e7b979b39775b
Ademir <ademir.m.junior@gmail.com>
2021-11-15 15:27:47 -0300

    client initial commit

--- server/src/server.js
+++ server/src/server.js
@@ -18,4 +18,4 @@ app.use(express.json());
 
 app.use('/api/v1/todos', ToDosRoutes);
 
-app.listen(3000, console.log("Listening on port 3000..."));
+app.listen(5000, console.log("Listening on port 5000..."));
